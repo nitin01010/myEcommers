1e78d1601ec22d98bec62553795a3e7775e2c689
nitin0202 <nitinsachdeva0202@gmail.com>
2024-07-24 22:28:17 +0530

    eCommerse width

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from '../Redux/store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className=' bg-white w-[100%] md:w-[80%] m-auto  '>
+  <div className=' bg-white w-[100%]   '>
     <BrowserRouter>
       <Provider store={ store }>
         <App />
